467fbd2dd5c2c8613394c9625b195010e26e9343
csq <402329396@qq.com>
2022-06-29 19:19:03 +0800

    .

--- Baidu_Text_transAPI.py
+++ Baidu_Text_transAPI.py
@@ -10,25 +10,26 @@ import random
 import json
 from hashlib import md5
 
+
 # Generate salt and sign
 def make_md5(s, encoding='utf-8'):
     return md5(s.encode(encoding)).hexdigest()
 
-def baiduTranslate(f,t,q):
+
+def baiduTranslate(f, t, q):
     # Set your own appid/appkey.
     appid = '20220628001259188'
     appkey = 'c8FbTwsW9QoG_7fzS8o5'
 
     # For list of language codes, please refer to `https://api.fanyi.baidu.com/doc/21`
     from_lang = f
-    to_lang =  t
+    to_lang = t
 
     endpoint = 'http://api.fanyi.baidu.com'
     path = '/api/trans/vip/translate'
     url = endpoint + path
 
     query = q
-
 
     salt = random.randint(32768, 65536)
     sign = make_md5(appid + query + str(salt) + appkey)
@@ -42,8 +43,9 @@ def baiduTranslate(f,t,q):
     result = r.json()
 
     # Show response
-    temp=result['trans_result'][0]['dst']
+    temp = result['trans_result'][0]['dst']
     return temp
 
+
 if __name__ == '__main__':
-    print(baiduTranslate('zh','en','为深入贯彻习总书记“疫情要防住、经济要稳住、发展要安全”的重要指示精神，全面落实全国稳住经济大盘电视电话会议部署要求，高效统筹疫情防控和经济社会发展，以更强的紧迫感、更高的主动性抢抓当前关键时间窗口，进一步推进知识产权政策实施提速增效，促进经济平稳健康发展'))
+    print(baiduTranslate('zh', 'en', '今天吃点什么'))
--- main.py
+++ main.py
@@ -1,58 +1,57 @@
-from nltk.translate.bleu_score import sentence_bleu,SmoothingFunction
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 import Baidu_Text_transAPI
 
 if __name__ == '__main__':
-    for i in range(35,41):
+    for i in range(35, 41):
         reference = [[]]
         candidate = []
-        a='data/original/'+str(i)+'.txt'
-        b='data/new/'+str(i)+'.txt'
-        c='data/result/'+str(i)+'.txt'
-        f=open(a,'r+',encoding='utf-8')
-        g=open(b,'w+',encoding='utf-8')
-        h=open(c,'w+',encoding='utf-8')
+        a = 'data/original/' + str(i) + '.txt'
+        b = 'data/new/' + str(i) + '.txt'
+        c = 'data/result/' + str(i) + '.txt'
+        f = open(a, 'r+', encoding='utf-8')
+        g = open(b, 'w+', encoding='utf-8')
+        h = open(c, 'w+', encoding='utf-8')
         for line in f.readlines():
-            line=line.strip()
-            line=line.replace('\u2003','')
-            line=line.replace(' ','')
-            line = line.replace('习近平总书记', '习总书记')
-            if line!='':
-                lines=line.split('。')
+            # 删除空格、回车、以及其他奇奇怪怪的多余字符
+            line = line.strip()
+            line = line.replace('\u2003', '')
+            line = line.replace(' ', '')
+            line = line.replace('习近平总书记', '习总书记')  # 这是由于百度自己算法内部的奇怪bug，如果不替换的话整句话的翻译不出来
+            if line != '':
+                # 把段落拆分成句子
+                lines = line.split('。')
                 for l in lines:
+                    # 继续删除多余字符
                     l = l.strip()
                     l = l.replace('\u2003', '')
                     l = l.replace(' ', '')
                     if l != '':
                         reference[0].append(l)
-        # for line in g.readlines():
-        #     line=line.strip()
-        #     line=line.replace('\u2003','')
-        #     line=line.replace(' ','')
-        #     if line!='':
-        #         candidate.append(line)
+        # 调用百度的翻译接口来实现回译
         for line in reference[0]:
-            temp = Baidu_Text_transAPI.baiduTranslate('zh','en',line)
-            temp = Baidu_Text_transAPI.baiduTranslate('en','zh',temp)
+            temp = Baidu_Text_transAPI.baiduTranslate('zh', 'en', line)
+            temp = Baidu_Text_transAPI.baiduTranslate('en', 'zh', temp)
             candidate.append(temp)
-            g.write(temp+'\n')
-        scores=[]
+            g.write(temp + '\n')
+        # 计算BLEU值
+        scores = []
         for j in range(len(candidate)):
-            s1=[reference[0][j]]
-            s2=candidate[j]
+            s1 = [reference[0][j]]
+            s2 = candidate[j]
+            # 使用smooth就可以不用把一整句话再切分成单个字
             smooth = SmoothingFunction()
-            if len(s2)<=4:
-                score=sentence_bleu(s1, s2, smoothing_function=smooth.method1,weights=(0.5,0.5,0,0))
-            elif len(s2)<=6:
-                score=sentence_bleu(s1, s2, smoothing_function=smooth.method1,weights=(0.33,0.33,0.33,0))
+            # 如果字数很少，就不计算3—gram和4-gram
+            if len(s2) <= 4:
+                score = sentence_bleu(s1, s2, smoothing_function=smooth.method1, weights=(0.5, 0.5, 0, 0))
+            elif len(s2) <= 6:
+                score = sentence_bleu(s1, s2, smoothing_function=smooth.method1, weights=(0.33, 0.33, 0.33, 0))
             else:
                 score = sentence_bleu(s1, s2, smoothing_function=smooth.method1)
             scores.append(score)
-            h.write("score"+str(j)+"="+str(score)+'\n')
-        score=sum(scores)/len(scores)
-        h.write("final result:"+str(score))
+            # 把结果写入另一个文档保存
+            h.write("score" + str(j) + "=" + str(score) + '\n')
+        score = sum(scores) / len(scores)
+        h.write("final result:" + str(score))
         f.close()
         g.close()
         h.close()
-
-
-
